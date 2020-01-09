a99c64b94e546a705f2f31a047d455d06c5b99b9
beston91 <beston91@github.com>
2020-01-09 16:03:58 -0500

    can you see if this is right?

--- +acrobot/acrobot_state_estimator.m
+++ +acrobot/acrobot_state_estimator.m
@@ -22,6 +22,7 @@ classdef acrobot_state_estimator < matlab.System
         prev_dist_to_floor = 0.0;
         state= [0;0;0;0];
         dir = true;
+        entered = false;
     end
 
     methods
@@ -52,7 +53,7 @@ classdef acrobot_state_estimator < matlab.System
             zyx = eulerd(orient,'ZYX','frame');
             zyx = deg2rad(zyx);
             % motor angle
-            qm = pi  - motor_step / (obj.steps_per_rotation/ 2* pi);
+            qm = pi  + motor_step / (obj.steps_per_rotation/ 2* pi);
             
             if(obj.dir)
                 % imu leg on the ground
@@ -69,7 +70,11 @@ classdef acrobot_state_estimator < matlab.System
                 q1_dot = -1*angVelocity(2);
                 q2 = pi - qm;
                 q2_dot = (qm - obj.state(2))/obj.sample_time;
-                q1 = pi +(q2 - zyx(1));
+                if(qm > pi)
+                    q1 = pi -(qm - zyx(1));
+                else
+                    q1 = -(qm - zyx(1));
+                end
             end
 
             rH = obj.leg_length * [cos(q1); sin(q1)];
@@ -77,13 +82,17 @@ classdef acrobot_state_estimator < matlab.System
             dist_to_floor = rc2(2);
             delta_dist = dist_to_floor - obj.prev_dist_to_floor;
             obj.prev_dist_to_floor = dist_to_floor;
-            if  abs(dist_to_floor) < 0.01 && delta_dist < 0
-                fprintf("here");
-                obj.dir = ~obj.dir;
-                fprintf('%.4f', dist_to_floor);  
+            fprintf('%.4f\n', delta_dist);  
+            if  abs(dist_to_floor) < 0.01 && delta_dist<0
+                if (~obj.entered)
+                    fprintf("here");
+                    obj.dir = ~obj.dir;
+                    fprintf('%.4f', dist_to_floor);  
+                    obj.entered = true;
+                end
             end
             
-            state = [q1;q2;q1_dot;q2_dot];
+            state = [q1;q2;q1_dot;qm];
             obj.state = state;
             return;
         end
