5802a8d12b40fffdc3c974cbc4cc2cc2444d2e4e
Jason <vuwij@me.com>
2020-01-21 17:42:04 -0500

    Revert "New stl files"
    This reverts commit 53de59f62c930bb24bb5f00c26a02f16f1bd15ca.

--- measurements/measure_imu.m
+++ measurements/measure_imu.m
@@ -1,19 +1,13 @@
-%% IMU Testing
+%% Arduino Rotary Encoder input
 clear; clc;
 a = arduino('/dev/ttyUSB0','Nano3','Libraries','I2C');
 imu = mpu6050(a);
 
-
 %% Count Encoder
-FUSE = imufilter();
 while(1)
-    
     acc = imu.readAcceleration;
     angvel = imu.readAngularVelocity;
-    [orient,angularVelocity] = FUSE(acc,angvel);
-    zyx = eulerd(orient,'ZYX','frame');
-    zyx = deg2rad(zyx);
-    fprintf('angle: %f %f %f\n', zyx(1), zyx(2), zyx(3));
-%     fprintf('Acc: %f %f %f Angvel: %f %f %f\n',acc(1), acc(2), acc(3), angvel(1), angvel(2), angvel(3))
-    pause(0.5);
+    
+    fprintf('Acc: %f %f %f Angvel: %f %f %f\n',acc(1), acc(2), acc(3), angvel(1), angvel(2), angvel(3))
+    pause(0.01);
 end
