a663920a60da767fece598067f8503428cc072d0
beston91 <beston91@gmail.com>
2020-03-09 13:02:39 -0400

    update

--- measurements/measure_imu.m
+++ measurements/measure_imu.m
@@ -1,31 +1,21 @@
 %% Arduino Rotary Encoder input
 clear; clc;
 a = arduino('COM4','Nano3','Libraries',{'Adafruit/BNO055', 'I2C'});
-BNO055Sensor = addon(a,'Adafruit/BNO055', 'I2CAddress', '0x28');
-BNO055Sensor2 = addon(a,'Adafruit/BNO055','I2CAddress','0x29');
-% 
-% 
-% 
-%% Count Encoder
-tic; 
-while (toc < 240)
-    [status,timestamp] = readCalibrationStatus(BNO055Sensor); 
-    status
-    if strcmpi(status.Accelerometer,'full') && strcmpi(status.System,'full')
-        break; %Accelerometer is calibrated proceed further
-    end
-    pause(1);
-end
+BNO055Sensor = addon(a,'Adafruit/BNO055', 'I2CAddress', '0x28','Bus',0);
+BNO055Sensor2 = addon(a,'Adafruit/BNO055','I2CAddress','0x29','Bus',0);
+
+%%
+
+c = 5;
+tstep = 0.1;  % Time step
+rate = rateControl(1/tstep);
+
 while (1)
-    [data,timestamp] = readOrientation(BNO055Sensor);
-%     pause(1);
+    [data,timestamp] = readAcceleration(BNO055Sensor);
+    [data2,timestamp] = readAcceleration(BNO055Sensor2);
+    
     data
+    data2
+    waitfor(rate);
 end
 
-% while(1)
-%     acc = imu.readAcceleration;
-%     angvel = imu.readAngularVelocity;
-%     
-%     fprintf('Acc: %f %f %f Angvel: %f %f %f\n',acc(1), acc(2), acc(3), angvel(1), angvel(2), angvel(3))
-%     pause(0.01);
-% end
